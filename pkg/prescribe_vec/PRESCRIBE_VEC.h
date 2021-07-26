33d60dde9e8081cacae8e37e2c75c2ccc0b15fdf
Mike Wood <mhwood@uci.edu>
2021-07-26 09:37:43 -0700

    some updates from the L1 test

--- pkg/prescribe_vec/PRESCRIBE_VEC.h
+++ pkg/prescribe_vec/PRESCRIBE_VEC.h
@@ -8,7 +8,7 @@ C------------------------------------------------------------------------------|
 C     Define the global prescribe_vec variables
 C------------------------------------------------------------------------------|
 
-      INTEGER, PARAMETER :: nVEC_masks = 8
+      INTEGER, PARAMETER :: nVEC_masks = 1
       INTEGER, PARAMETER :: N_VEC_FIELDS = 5 
 
 C     These are the name of the fields to be prescribed
@@ -16,6 +16,9 @@ C     These are the name of the fields to be prescribed
 
 C     These are the file names of the input masks
       CHARACTER*30 vec_fname_prefixes(nVEC_masks)
+
+C     These are the file names for the depth level option
+      CHARACTER*30 vec_depth_cell_fnames(N_VEC_FIELDS,nVEC_masks)
 
 C     These are the file names for the relaxation option
       CHARACTER*30 vec_relax_time_fnames(nVEC_masks)
@@ -41,12 +44,17 @@ C     These are some i/o parameters to help read in the fields
       INTEGER vec_filePrec
       INTEGER nTimesteps_vec
       LOGICAL vec_fields_include_ICs
+      LOGICAL vec_field_is_constant
 
 C     This an option to either prescribe inside forward_step (vs elsewhere?)
       LOGICAL prescribe_in_forward_step
 
 C     This is where the input masks are stored after they are read in
       _RL vec_subMask(nVEC_masks,1-Olx:sNx+Olx,1-Oly:sNy+Oly,nSx,nSy)
+
+C     This is where the depth levels are stored when they are read in
+C      - these may be provided by a file or a list
+      _RL depth_mask(nVEC_masks,1-Olx:sNx+Olx,1-Oly:sNy+Oly,nSx,nSy)
 
 C     This is where the relaxation timescales are stored
 C      - these may be provided by a file or stored as 0 by dafault 
@@ -58,6 +66,9 @@ C      - this may be provided by a file or stored as 0 by dafault
 
 C     This is where the local row and col of the location in the tile are stored
       INTEGER vec_point_ij(nVEC_masks, 4, sNy*sNx)
+
+C     This is where the local depth levels in the tile are stored
+      INTEGER vec_depth_level(nVEC_masks, N_VEC_FIELDS, sNy*sNx)
 
 C     These are dictionaries linking the mask counter to its proc
 C           tile, and the row/col within that tile
@@ -77,6 +88,7 @@ C------------------------------------------------------------------------------|
 
       COMMON / DIAG_VEC_VARS_R /
      &     vec_subFields, vec_subMask,
+     &     depth_mask,
      &     relax_time, relax_mask
 
       COMMON / DIAG_VEC_VARS_I /
@@ -85,14 +97,17 @@ C------------------------------------------------------------------------------|
      &     vec_nFlds,
      &     vec_filePrec,nTimeSteps_vec,
      &     vec_mask_index_list,vec_point_ij,
+     &     vec_depth_level,
      &     vec_nRelaxPoints, vec_nPrescribePoints
 
       COMMON / DIAG_VEC_VARS_C /
      &     vec_flds, vec_fname_prefixes,
-     &     vec_relax_time_fnames, vec_relax_mask_fnames
+     &     vec_relax_time_fnames, vec_relax_mask_fnames,
+     &     vec_depth_cell_fnames
 
       COMMON / DIAG_VEC_VARS_L /
      &     prescribe_in_forward_step,
-     &     vec_fields_include_ICs
+     &     vec_fields_include_ICs,
+     &     vec_field_is_constant
 
 #endif /* ALLOW_DIAGNOSTICS_VEC */
