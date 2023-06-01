678b7298f2d81d499ec390184201ed44eb74cb34
Mike Wood <46327239+mhwood@users.noreply.github.com>
2023-05-31 17:14:17 -0700

    Update DIAGNOSTICS_VEC_SIZE.h

--- example_configurations/global_with_exf/global/code/DIAGNOSTICS_VEC_SIZE.h
+++ example_configurations/global_with_exf/global/code/DIAGNOSTICS_VEC_SIZE.h
@@ -1,6 +1,7 @@
 C------------------------------------------------------------------------------|
 C                           DIAGNOSTICS_VEC_SIZE.h
 C------------------------------------------------------------------------------|
-
+      
+      INTEGER, PARAMETER :: VEC_points = 100
       INTEGER, PARAMETER :: nVEC_mask = 12
       INTEGER, PARAMETER :: nSURF_mask = 1
